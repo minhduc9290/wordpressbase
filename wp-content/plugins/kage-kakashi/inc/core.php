4dd753eb67499558d08d15256ae54f996b063be1
DucPv <ducpv@steadfast-inc.com>
2018-01-03 16:33:44 +0700

    update kage-kakashi

---
+++ wp-content/plugins/kage-kakashi/inc/core.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * @version    1.0
+ * @package    Kage_Kakashi
+ * @author     Mr-DucPham <minhduc9290@gmail.com>
+ * @copyright  Copyright (C) 2014 Ducpham.com. All Rights Reserved.
+ * @license    GNU/GPL v2 or later http://www.gnu.org/licenses/gpl-2.0.html
+ *
+ * Websites: http://www.ducpham.com
+ */
+
+ Class Kage_Kakashi {
+     /**
+ 	 * Variable to hold class prefix supported for autoloading.
+ 	 *
+ 	 * @var  string
+ 	 */
+     protected $prifix = 'kage-kakashi';
+     /**
+      * initialize
+      */
+     public static function initialize(){
+
+     }
+
+ }
+?>
--- wp-content/plugins/kage-kakashi/kage-kakashi.php
+++ wp-content/plugins/kage-kakashi/kage-kakashi.php
@@ -10,4 +10,15 @@
  * Text Domain: kage-kakashi
  */
 
+ // Define path to plugin directory.
+ define( 'KAGE_KAKASHI_PATH', plugin_dir_path( __FILE__ ) );
+
+ // Define URL to plugin directory.
+ define( 'KAGE_KAKASHI_URL', plugin_dir_url( __FILE__ ) );
+
+ // Load the core class.
+ require_once KAGE_KAKASHI_PATH . 'inc/core.php';
+
+ // Instantiate an object of the core class.
+ Kage_Kakashi::initialize();
  ?>
